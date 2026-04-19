06cafaeff368b394e5c9eb77169f0d5534595024
SHRI DAYAL VERMA <shridayalverma896@gmail.com>
2026-04-19 18:10:24 +0530

    updated files

--- AWT/p1.java
+++ AWT/p1.java
@@ -1,29 +1,13 @@
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-class p1 extends Frame{
-    p1(String name){
-       super(name);
-       TextField t = new TextField();
-       t.setBounds(20,120,150,30);
-
-       Label l = new Label("Hello Friends : ");
-       l.setBounds(20,90,150,30);
-
-       Button b = new Button("Click Me!");
-       b.setBackground(Color.green);
-       b.setForeground(Color.white);
-       b.setBounds(20,40,80,30);
-
-       add(t);
-       add(l);
-       add(b);
-
+class  p1 extends Frame{
+    p1(String s){
+        super(s);
     }
     public static void main(String[] args) {
         p1 obj = new p1("SDV");
-        obj.setSize(500,500);
-        obj.setLayout(null);
+        obj.setSize(400,400);
         obj.setVisible(true);
         obj.addWindowListener(new WindowAdapter(){
             @Override
