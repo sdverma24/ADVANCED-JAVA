2dcbd7c17d2792de93c5c47ffcc3c8b03771e48b
SHRI DAYAL VERMA <shridayalverma896@gmail.com>
2026-05-07 18:53:03 +0530

    updated files

--- AWT/p1.java
+++ AWT/p1.java
@@ -1,13 +1,16 @@
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-class  p1 extends Frame{
-    p1(String s){
+class p1 extends Frame
+{
+    p1(String s)
+    {
         super(s);
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         p1 obj = new p1("SDV");
-        obj.setSize(700,400);
+        obj.setSize(300,400);
         obj.setVisible(true);
         obj.addWindowListener(new WindowAdapter(){
             @Override
