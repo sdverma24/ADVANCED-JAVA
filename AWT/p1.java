f2059297a23794a77eecab5585388573a9ad1937
SHRI DAYAL VERMA <shridayalverma896@gmail.com>
2026-05-11 16:51:53 +0530

    updated files

--- AWT/p1.java
+++ AWT/p1.java
@@ -1,16 +1,13 @@
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-class p1 extends Frame
+import java.awt.event.WindowListener;
+class p1 extends Frame implements WindowListener
 {
     p1(String s)
     {
         super(s);
-    }
-    public void windowClosing(WindowEvent e)
-    {
-       Frame frame = (Frame)e.getSource();
-       frame.dispose();
+        addWindowListener(this);
     }
     public static void main(String args[])
     {
@@ -28,5 +25,40 @@ class p1 extends Frame
         Button b = new Button("oK");
         b.setBounds(20,20,100,100);
         obj.add(b);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        System.out.println("Window Closed");
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        System.out.println("Window Iconified");
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        System.out.println("Window Deiconified");
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        System.out.println("Window Activated");
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        System.out.println("Window Deactivated");
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        System.out.println("Window Opened");
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.out.println("Closing Window");
     }
 }
